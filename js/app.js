0645e8b2f7467b7c4eba3086e4568853540ff41d
StefaniaVitali <stefy.egypt@hotmail.it>
2024-03-24 14:21:46 +0100

    ADD: html and js file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log('JS-Oggetti_Studenti')
+
+/* CONSEGNA
+MILESTONE 1
+Creare un array di oggetti di studenti.
+Ogni oggetto studente deve avere tre proprietà: nome, cognome, eta
+Ciclare su tutti gli studenti e stampare, per ognuno di essi, nome e cognome.
+prima ciclate con un ciclo for
+poi commentate il ciclo for e usate il forEach
+MILESTONE 2
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+Aggiungere il nuovo studente all’array di studenti
+Ciclare su tutti gli studenti e stampare per ognuno di essi tutte le proprietà nome, cognome, eta
+prima ciclate con un ciclo for
+poi commentate il ciclo for e usate il forEach
+ */
