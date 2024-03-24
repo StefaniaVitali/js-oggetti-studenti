6f47bfeb50201d9c18f878630365955a03c49ea9
StefaniaVitali <stefy.egypt@hotmail.it>
2024-03-24 15:23:31 +0100

    ADD: forEach and comments

--- js/app.js
+++ js/app.js
@@ -6,14 +6,7 @@ Creare un array di oggetti di studenti.
 Ogni oggetto studente deve avere tre proprietà: nome, cognome, eta
 Ciclare su tutti gli studenti e stampare, per ognuno di essi, nome e cognome.
 prima ciclate con un ciclo for
-poi commentate il ciclo for e usate il forEach
-MILESTONE 2
-Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-Aggiungere il nuovo studente all’array di studenti
-Ciclare su tutti gli studenti e stampare per ognuno di essi tutte le proprietà nome, cognome, eta
-prima ciclate con un ciclo for
-poi commentate il ciclo for e usate il forEach
- */
+poi commentate il ciclo for e usate il forEach */
 
 
 //creare un array di oggetti di studenti   
@@ -52,6 +45,8 @@ const studenti = [
 
 console.log(studenti);
 
+//
+
 //ciclare tutti gli studenti e stampare nome e cognome
 // for (let i = 0; i < studenti.length; i++){
 
@@ -72,15 +67,46 @@ studenti.forEach(function(studente,i){
     //--costante nome
     const name = studente.nome
     //--costante cognome
-   const surname = studente.cognome  
-   
+    const surname = studente.cognome     
    //stampare nome e cognome
-   console.log(`${name} ${surname}`)  
+    console.log(`${name} ${surname}`)  
 });
 
 //perché nel ciclo for ho dovuto dare un valore alla variabile i (let studente = studenti[i]),
 //mentre nel forEach non serve? Perché avendolo indicato nel paramentro è già di suo il singolo
 //elemento dell'array?
+
+
+/* MILESTONE 2
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+Aggiungere il nuovo studente all’array di studenti
+Ciclare su tutti gli studenti e stampare per ognuno di essi tutte le proprietà nome, cognome, eta
+prima ciclate con un ciclo for
+poi commentate il ciclo for e usate il forEach */
+
+
+//creare oggetto studente 
+const nuovoStudente = 
+             //chiedere tramite prompt nome
+            {nome: prompt('Inserisci il nome'), //string | Null
+             //chiedere tramite prompt cognome
+             cognome: prompt('Inserisci il cognome'), //string | Null
+             //chiedere tramite prompt età e trasformare stringa in numero
+             eta: parseInt(prompt('Inserisci la tua età')) //Number | NaN
+             };
+console.log(nuovoStudente)
+
+studenti.push(nuovoStudente);
+console.log(studenti);
+
+// for (let i = 0; i<studenti.length; i++){
+//     let studente = studenti[i];
+//     console.log(`${studente.nome} ${studente.cognome} ${studente.eta}`)  
+// };
+
+studenti.forEach(function(studente, i){
+    console.log(`${studente.nome} ${studente.cognome} ${studente.eta}`) 
+});
 
 
 
