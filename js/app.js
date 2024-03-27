295794a254ac232d43965754d59b984ba03eaf13
StefaniaVitali <stefy.egypt@hotmail.it>
2024-03-27 18:36:55 +0100

    ADD table

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ const studenti = [
     {
         nome: 'Stefania',
         cognome: 'Satta',
-        eta: 23,
+        eta: 13,
     },
     {
         nome: 'Luciano',
@@ -32,7 +32,7 @@ const studenti = [
     {
         nome: 'Carlo',
         cognome: 'Scano',
-        eta: 21,
+        eta: 17,
     },
     {
         nome: 'Ilaria',
@@ -96,29 +96,32 @@ poi commentate il ciclo for e usate il forEach */
 //creare oggetto studente vuoto => provare in maniera dinamica
 const nuovoStudente = {};
 
+//INIZIA LA RICHIESTA TRAMITE PROMPT
 
 // chiedere tramite prompt nome
-let   propName = 'nome'
-const valueName = prompt('Inserisci il nome'); //string | Null
+// let   propName = 'nome'
+// const valueName = prompt('Inserisci il nome'); //string | Null
 
-//chiedere tramite prompt cognome
-let propSurname = 'cognome'
-const valueSurname = prompt('Inserisci il cognome');//string | Null
+// //chiedere tramite prompt cognome
+// let propSurname = 'cognome'
+// const valueSurname = prompt('Inserisci il cognome');//string | Null
 
-//chiedere tramite prompt età e trasformare stringa in numero
-let propEta = 'eta'
-const valueEta = parseInt(prompt('Inserisci la tua età'));
+// //chiedere tramite prompt età e trasformare stringa in numero
+// let propEta = 'eta'
+// const valueEta = parseInt(prompt('Inserisci la tua età'));
+
 
 
 
 // creare le proprietà da inserire nell'oggetto studente
-nuovoStudente[propName] = valueName;
-nuovoStudente[propSurname] = valueSurname;
-nuovoStudente[propEta] = valueEta;
+// nuovoStudente[propName] = valueName;
+// nuovoStudente[propSurname] = valueSurname;
+// nuovoStudente[propEta] = valueEta;
 
-console.log(nuovoStudente)
+// console.log(nuovoStudente)
+// studenti.push(nuovoStudente);
+//FINISCE LA RICHIESTA TRAMITE PROMPT
 
-studenti.push(nuovoStudente);
 // console.log(studenti);
 
 //per ogni oggetto studente dell'array ciclare e ottenere nome cognome ed età
@@ -179,7 +182,7 @@ console.log(inputNome, inputCognome, inputEta, buttonElement)
 // });
 
 //PROVARE CON IL TAG FORM
-formElement.addEventListener('submit', function(event){
+formElement.addEventListener('submit', function (event) {
     event.preventDefault()
 
     let propName = 'nome'
@@ -198,15 +201,99 @@ formElement.addEventListener('submit', function(event){
     nuovoStudente[propEta] = valueEta2;
 
     console.log(nuovoStudente)
-
     studenti.push(nuovoStudente);
     // console.log(studenti);
 
     studenti.forEach(function (studente, i) {
-    console.log(`${studente.nome} ${studente.cognome} ${studente.eta}`)
+        console.log(`${studente.nome} ${studente.cognome} ${studente.eta}`)
     });
 
-})
+});
+
+//// SECONDA PARTE 
+
+/* CONSEGNA
+MILESTONE 1
+Creare un array di oggetti di studenti.  =>(C'è GIà)
+Ogni oggetto studente deve avere tre proprietà:nome, cognome, eta .=> (C'è GIà)
+Ciclare su tutti gli studenti e stampare, per ognuno di essi, nome, cognome, età   
+tramite sfruttando i componenti bootstrap (ad es. in tabella).*/
+
+//andiamo a prendere l'elemento html dove stampiamo gli studenti
+const tbodyElement = document.getElementById('tbody-studenti');
+const tableElement = document.getElementById('table-studenti');
+console.log(tbodyElement);
+
+//cicliamo:
+// PER OGNI studente prendiamo nome, cognome ed età e lo stampiamo nella tabella
+studenti.forEach(function (studente) {
+    tbodyElement.innerHTML += ` <tr>
+    <td>${studente.nome}</td>
+    <td>${studente.cognome}</td>
+    <td>${studente.eta}</td>
+ </tr>`
+});
+
+/* MILESTONE 2
+Dare la possibilità all’utente di aggiungere un nuovo oggetto studente 
+inserendo nome, cognome ed età tramite form.
+Aggiungere il nuovo studente all’array di studenti.
+Aggiornare l’interfaccia con il nuovo studente.*/
+
+formElement.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    let propName = 'nome'
+    const valueName2 = inputNome.value //string | Null
+
+    let propSurname = 'cognome'
+    const valueSurname2 = inputCognome.value //string | Null
+
+    let propEta = 'eta'
+    const valueEta2 = parseInt(inputEta.value);//number
+
+    // console.log(valueName2, valueSurname2, valueEta2);
+
+    nuovoStudente[propName] = valueName2;
+    nuovoStudente[propSurname] = valueSurname2;
+    nuovoStudente[propEta] = valueEta2;
+
+    console.log(nuovoStudente)
+    studenti.push(nuovoStudente);
+    // console.log(studenti);
+
+    tbodyElement.innerHTML += ` <tr>
+       <td>${nuovoStudente.nome}</td>
+       <td>${nuovoStudente.cognome}</td>
+       <td>${nuovoStudente.eta}</td>
+      </tr>`
+    /*BONUS
+    Mostrare solo gli studenti maggiorenni filtrando gli elementi dell’array 
+    prima di stampare la nuova lista in pagina*/
+
+    tbodyElement.innerHTML = ""
+
+    studenti.forEach(function (studente, i, studenti) {
+        //verificare la maggiore età
+        console.log(studente, i)
+        if (studente.eta >= 18) {
+
+            console.log('sono maggiorenne')
+            //se maggiorenne stampare la nuova lista       
+            tbodyElement.innerHTML += ` <tr>
+       <td>${studente.nome}</td>
+       <td>${studente.cognome}</td>
+       <td>${studente.eta}</td>
+      </tr>`
+
+        }
+    })
+});
+
+
+
+
+
 
 
 
